7a81311a4d7427650d69022151efc6b94a726e7d
Agustin Manuel Gutierrez <agustinguti123@gmail.com>
2022-01-07 20:40:21 -0300

    cambios ponele

--- src/01-lazyload/layout/LazyLayout.tsx
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Navigation } from "../router/Navigation";
 
 export const LazyLayout = () => {
@@ -8,5 +9,18 @@ export const LazyLayout = () => {
     </div>
   );
 };
+=======
+import { Navigation } from '../router/Navigation';
+
+export const LazyLayout = () => {
+    return (
+        <div>
+            <h1>LazyLayout - Main Page</h1>
+            <Navigation />
+        </div>
+    )
+}
+
+>>>>>>> 974413b64e35c00c615f3e5e800f29cbedc10a4c
 
 export default LazyLayout;
--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -1,5 +1,16 @@
+<<<<<<< HEAD
 export const LazyPage1 = () => {
   return <h1>LazyPage1</h1>;
 };
+=======
+
+
+export const LazyPage1 = () => {
+    return (
+        <h1>LazyPage1</h1>
+    )
+}
+
+>>>>>>> 974413b64e35c00c615f3e5e800f29cbedc10a4c
 
 export default LazyPage1;

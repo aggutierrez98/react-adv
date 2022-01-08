8f58a8b9b7e850ff703dc4c1d9c92ea582409159
Agustin Manuel Gutierrez <agustinguti123@gmail.com>
2022-01-08 17:54:26 -0300

    Component compound listo

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,31 @@
+import styles from "../styles/styles.module.css";
+import { useProduct } from "../hooks/useProduct";
+import { createContext } from "react";
+import {
+  ProductContextProps,
+  ProductCardProps,
+} from "../interfaces/interfaces";
+import { ProductButtons, ProductImage, ProductTitle } from ".";
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+export const ProductCard = ({ product, children }: ProductCardProps) => {
+  const { counter, increaseBy } = useProduct();
+
+  return (
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        product,
+      }}
+    >
+      <div className={styles.productCard}>{children}</div>
+    </Provider>
+  );
+};
+
+ProductCard.Title = ProductTitle;
+ProductCard.Image = ProductImage;
+ProductCard.Buttons = ProductButtons;
